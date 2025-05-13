0b84f11b4e664d7505056c8e428b8f5e0b143f4a
Thanklearncode <Thanhhh2005@gmail.com>
2025-05-13 13:05:32 +0700

    add first-weekPractice

---
+++ app/src/main/java/com/example/baitaptuan1/MainActivity2.kt
@@ -0,0 +1,25 @@
+package com.example.baitaptuan1
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        val imgBtn_arrowBack = findViewById<ImageButton>(R.id.imgBtn_arrowBack)
+        imgBtn_arrowBack.setOnClickListener {
+            finish()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
